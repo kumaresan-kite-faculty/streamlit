942ed246af155235fb501eedde549aa83f5b6607
kumaresan-kite-faculty <kumaresan.m@kgkite.ac.in>
2024-12-24 19:28:43 +0530

    updated

--- streamlit_example.py
+++ streamlit_example.py
@@ -1,2 +1,10 @@
 import streamlit as st
 st.write("Hello World")
+def add():
+    num1=st.number_input("enter number 1")
+    num2=st.number_input("enternumber 2")
+    if st.button("ADD"):
+        num3=num1+num2
+        st.success(f"The addition of {num1} and {num2} is {num3}")
+if __name__=="__main__":
+    add()
