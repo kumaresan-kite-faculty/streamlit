6e4060d95feeb0a9de92f5c105c430e5fc0d2231
kumaresan-kite-faculty <kumaresan.m@kgkite.ac.in>
2024-12-24 19:43:36 +0530

    modified with html code

--- streamlit_example.py
+++ streamlit_example.py
@@ -1,10 +1,14 @@
 import streamlit as st
 st.write("Hello World")
+st.title("KGiSL Institute of Technology")
 def add():
     num1=st.number_input("enter number 1")
     num2=st.number_input("enternumber 2")
     if st.button("ADD"):
         num3=num1+num2
         st.success(f"The addition of {num1} and {num2} is {num3}")
+        st.html(
+            "<marquee>This is my new code slider</marquee>"
+        )
 if __name__=="__main__":
     add()
